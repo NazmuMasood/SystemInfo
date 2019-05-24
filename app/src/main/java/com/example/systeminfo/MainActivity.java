20a2c2daa47f3d3bf747aa2af255b242134d2b78
NazmuMasood <44304205+NazmuMasood@users.noreply.github.com>
2019-05-25 00:10:22 +0600

    code cleaning performed

--- app/src/main/java/com/example/systeminfo/MainActivity.java
+++ app/src/main/java/com/example/systeminfo/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.systeminfo;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.BatteryManager;
@@ -11,7 +13,6 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     ImageView ivBattery; TextView tvBattery;
-    Handler handler; Runnable runnable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,41 +21,30 @@ public class MainActivity extends AppCompatActivity {
 
         ivBattery = findViewById(R.id.ivBattery);
         tvBattery = findViewById(R.id.tvBattery);
+        this.registerReceiver(this.batteryInfoReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+    }
 
-        runnable = new Runnable() {
-            @Override
-            public void run() {
-                int level = (int) batteryLevel();
-                tvBattery.setText("BATTERY: "+level+"%");
+    private BroadcastReceiver batteryInfoReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
+            tvBattery.setText("BATTERY: "+level+"%");
 
-                if (level>75){
-                    ivBattery.setImageResource(R.drawable.battery_100);
-                }
-                if (level>50 && level<=75){
-                    ivBattery.setImageResource(R.drawable.battery_75);
-                }
-                if (level>25 && level<=50){
-                    ivBattery.setImageResource(R.drawable.battery_50);
-                }
-                if (level>5 && level<=25){
-                    ivBattery.setImageResource(R.drawable.battery_25);
-                }
-                if (level<=5){
-                    ivBattery.setImageResource(R.drawable.battery_0);
-                }
+            if (level>75){
+                ivBattery.setImageResource(R.drawable.battery_100);
             }
-        };
-        handler = new Handler();
-        handler.postDelayed(runnable, 0);
-    }
-
-    public float batteryLevel(){
-        Intent batteryIntent = registerReceiver(null,new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
-        int level = batteryIntent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
-        int scale = batteryIntent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
-        if (level == -1 || scale == -1){
-            return 50.0f;
+            if (level>50 && level<=75){
+                ivBattery.setImageResource(R.drawable.battery_75);
+            }
+            if (level>25 && level<=50){
+                ivBattery.setImageResource(R.drawable.battery_50);
+            }
+            if (level>5 && level<=25){
+                ivBattery.setImageResource(R.drawable.battery_25);
+            }
+            if (level<=5){
+                ivBattery.setImageResource(R.drawable.battery_0);
+            }
         }
-        return ((float) level / (float) scale) * 100.0f;
-    }
+    };
 }
